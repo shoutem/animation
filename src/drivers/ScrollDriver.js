555aa6144f0c518551d93abfc29cdd1c98cdf539
Željko Rumenjak <zeljko@shoutem.com>
2017-03-07 19:22:30 +0100

    Native animation support on ScrollDriver

--- src/drivers/ScrollDriver.js
+++ src/drivers/ScrollDriver.js
@@ -1,4 +1,7 @@
 import { Animated } from 'react-native';
+import _ from 'lodash';
+
+import { DriverBase } from './DriverBase';
 
 /**
  * Animation driver that creates animated value changed on scroll event.
@@ -10,21 +13,39 @@ import { Animated } from 'react-native';
  *  driver
  * });
  * ...
- * <ScrollView onScroll={onScroll()}>
+ * <ScrollView onScroll={onScroll}>
+ *
+ * This driver initializes a native animated value by default, but
+ * it also creates a mirrored JS value as a fallback for animations
+ * that are not yet supported in native. The users can control if
+ * interpolations will be performed in native or not by passing
+ * `useNativeDriver` in the interpolation options.
+ *
+ * @param options Driver options
+ * @param options.useNativeDriver Determines if the native animated value is used.
+ * @param options.nativeScrollEventThrottle Native animated value changes
+ *   will be debounced using this value when mirroring them to the JS value.
+ *   Used only if `useNativeDriver` is `true`, defaults to 20ms.
  */
-export class ScrollDriver {
-  constructor() {
-    this.value = new Animated.Value(0);
+export class ScrollDriver extends DriverBase {
+  constructor(options = { useNativeDriver: true, nativeScrollEventThrottle: 20 }) {
+    super();
+
+    if (options.useNativeDriver) {
+      this.nativeValue = new Animated.Value(0);
+
+      this.nativeValue.addListener(_.debounce(({ value }) => {
+        this.value.setValue(value);
+      }), options.nativeScrollEventThrottle);
+    }
+
+    this.primaryValue = this.nativeValue || this.value;
+
     this.onScrollViewLayout = this.onScrollViewLayout.bind(this);
     this.scrollViewProps = {
       onScroll: Animated.event(
-        [{
-          nativeEvent: {
-            contentOffset: {
-              y: this.value,
-            },
-          },
-        }]
+        [{ nativeEvent: { contentOffset: { y: this.primaryValue } } }],
+        { useNativeDriver: options.useNativeDriver }
       ),
       scrollEventThrottle: 1,
       onLayout: this.onScrollViewLayout,
@@ -33,5 +54,28 @@ export class ScrollDriver {
 
   onScrollViewLayout(event) {
     this.layout = event.nativeEvent.layout;
+  }
+
+  /**
+   * Creates an interpolation using the underlying animated
+   * value. This function will use the native animated value
+   * for the animation, is possible.
+   *
+   * @param config The animated interpolation config that may
+   *   define an additional `useNativeDriver` property which
+   *   controls whether this animation can be executed using
+   *   the native driver.
+   * @returns {*} The animated interpolation.
+   */
+  interpolate(config) {
+    let value = this.value;
+    if (config.useNativeDriver && this.nativeValue) {
+      value = this.nativeValue;
+    }
+
+    const interpolationConfig = { ...config };
+    delete interpolationConfig.useNativeDriver;
+
+    return value.interpolate(interpolationConfig);
   }
 }
